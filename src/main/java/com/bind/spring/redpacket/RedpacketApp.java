0cb857e4aa2d8eb8e48a7cb5891f05fa9427db98
careybind <821874441@qq.com>
2019-03-20 13:40:12 +0800

    the first

---
+++ src/main/java/com/bind/spring/redpacket/RedpacketApp.java
@@ -0,0 +1,12 @@
+package com.bind.spring.redpacket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedpacketApp {
+	public static void main(String[] args) {
+		SpringApplication.run(RedpacketApp.class, args);
+	}
+
+}
